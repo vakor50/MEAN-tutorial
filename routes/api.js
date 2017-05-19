9e2ec263d98bb86d8b8a76b19880ffe8a135db62
Vir Thakor <thakor.vir@gmail.com>
2017-05-18 21:59:35 -0700

    tutorial

---
+++ routes/api.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var router = express.Router();
+
+router.use(function(req, res, next) {
+
+	if (req.method == "GET") {
+		// continue to the next middleware or request handler
+		return next();
+	}
+	if (!req.isAuthenticated()) {
+		// user not authenticated, redirect to login page
+		return res.redirect('/#login');
+	}
+
+	// user authenticated continue to next middleware or handler
+	return next();
+});
+router.route('/posts')
+	
+	// returns all posts
+	.get(function (req, res) {
+
+		// temporary solution
+		res.send({message: 'TODO return all posts'});
+	})
+
+	.post(function (req, res) {
+
+		// temporary solution
+		res.send({message: 'TODO create a new post'});
+	});
+
+router.route('/posts/:id')
+
+	// returns a particular post
+	.get(function (req, res) {
+		
+		res.send({message: 'TODO return post with ID  ' + req.params.id});
+	})
+
+	// updates existing post
+	.put(function (req, res) {
+
+		res.send({message: 'TODO modify post with ID  ' + req.params.id});
+	})
+
+	// delete existing post
+	.delete(function(req, res) {
+
+		res.send({message: 'TODO delete post with ID  ' + req.params.id});
+	});
+
+module.exports = router;
---
+++ routes/authenticate.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+
+module.exports = function(passport){
+
+    //sends successful login state back to angular
+    router.get('/success', function(req, res){
+        res.send({state: 'success', user: req.user ? req.user : null});
+    });
+
+    //sends failure login state back to angular
+    router.get('/failure', function(req, res){
+        res.send({state: 'failure', user: null, message: "Invalid username or password"});
+    });
+
+    //log in
+    router.post('/login', passport.authenticate('login', {
+        successRedirect: '/auth/success',
+        failureRedirect: '/auth/failure'
+    }));
+
+    //sign up
+    // call authenticate strategy of signup
+    // either redirects client to success or failure
+    router.post('/signup', passport.authenticate('signup', {
+        successRedirect: '/auth/success',
+        failureRedirect: '/auth/failure'
+    }));
+
+    //log out
+    router.get('/signout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+    return router;
+
+}
